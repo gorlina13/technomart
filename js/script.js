11fad726ff91ea95ebb27747d065a8f409d02e43
Olga Bazhenova <olgabazhenova07@yandex.ru>
2018-07-21 20:28:44 +0300

    Сверстана страница каталога, скрипт для модальных окон дополнен

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var mapLink = document.querySelector('.contacts__map');
-var contactsButton = document.querySelector('.contacts__button');
+var messageButton = document.querySelector('.contacts__button');
+var toCartButtons = document.querySelectorAll('.button--buy');
 var mapModal = document.querySelector('.modal--map');
 var messageModal = document.querySelector('.modal--message');
+var cartPlusModal = document.querySelector('.modal--cart-plus');
 var modals = document.querySelectorAll('.modal');
 
 function showModal(event, modal) {
@@ -17,15 +19,23 @@ function hideModal(modal) {
   }
 }
 
-if (mapLink) {
+if ((mapLink) && (mapModal)) {
   mapLink.addEventListener('click', function (event) {
     showModal(event, mapModal);
   });
 }
 
-if (contactsButton) {
-  contactsButton.addEventListener('click', function (event) {
+if ((messageButton) && (messageModal)) {
+  messageButton.addEventListener('click', function (event) {
     showModal(event, messageModal);
+  });
+}
+
+if ((toCartButtons) && (cartPlusModal)) {
+  [].forEach.call(toCartButtons, function (item) {
+    item.addEventListener('click', function (event) {
+      showModal(event, cartPlusModal);
+    });
   });
 }
 
@@ -44,8 +54,16 @@ if (modals) {
 }
 
 function initMap() {
-  var myLatLng = {lat: 59.938549, lng: 30.322993};
-  var myBigCenter = {lat: 59.9395147, lng: 30.3151553};
+  var myLatLng = {
+    lat: 59.938549,
+    lng: 30.322993
+  };
+
+  var myBigCenter = {
+    lat: 59.9395147,
+    lng: 30.3151553
+  };
+
   var myPreviewOptions = {
     zoom: 16,
     center: myLatLng,
@@ -58,19 +76,25 @@ function initMap() {
     scrollwheel: false
   };
 
-  var mapPreview = new google.maps.Map(document.querySelector('.map__interactive-area--preview'), myPreviewOptions);
+  var mapCanvas1 = document.querySelector('.map__interactive-area--preview');
+  var mapCanvas2 = document.querySelector('.map__interactive-area--big');
 
-  var marker1 = new google.maps.Marker({
-    position: myLatLng,
-    map: mapPreview,
-    title: 'Техномарт'
-  });
+  if (mapCanvas1) {
+    var mapPreview = new google.maps.Map(mapCanvas1, myPreviewOptions);
 
-  var bigMap = new google.maps.Map(document.querySelector('.map__interactive-area--big'), myBigMapOptions);
+    var marker1 = new google.maps.Marker({
+      position: myLatLng,
+      map: mapPreview,
+      title: 'Техномарт'
+    });
+  }
 
-  var marker2 = new google.maps.Marker({
-    position: myLatLng,
-    map: bigMap,
-    title: 'Техномарт'
-  });
+  if (mapCanvas2) {
+    var bigMap = new google.maps.Map(mapCanvas2, myBigMapOptions);
+    var marker2 = new google.maps.Marker({
+      position: myLatLng,
+      map: bigMap,
+      title: 'Техномарт'
+    });
+  }
 }
