761c17ad776840cbbcf078cac556e851577df01f
Olga Bazhenova <olgabazhenova07@yandex.ru>
2018-07-24 18:31:33 +0300

    Для модальных окон добавлена анимация, скрипт переработан и дополнен

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 'use strict';
 
 var mapLink = document.querySelector('.contacts__map');
-var messageButton = document.querySelector('.contacts__button');
-var toCartButtons = document.querySelectorAll('.button--buy');
 var mapModal = document.querySelector('.modal--map');
+var messageButton = document.querySelector('.contacts__button');
 var messageModal = document.querySelector('.modal--message');
+var toCartButtons = document.querySelectorAll('.button--buy');
 var cartPlusModal = document.querySelector('.modal--cart-plus');
 var modals = document.querySelectorAll('.modal');
+var overlay = document.querySelector('.overlay');
+var messageForm = messageModal.querySelector('.message-form');
+var username = messageModal.querySelector('[name=username]');
+var email = messageModal.querySelector('[name=email]');
+var storage = localStorage.getItem('username');
 
 function showModal(event, modal) {
   event.preventDefault();
+  if (overlay) {
+    overlay.classList.add('overlay--show');
+  }
   modal.classList.add('modal--show');
 }
 
 function hideModal(modal) {
-  if (modal.classList.contains('modal--show')) {
-    modal.classList.remove('modal--show');
+  modal.classList.remove('modal--show');
+  modal.classList.remove('modal--error');
+  if (overlay) {
+    overlay.classList.remove('overlay--show');
   }
 }
 
@@ -28,6 +38,23 @@ if ((mapLink) && (mapModal)) {
 if ((messageButton) && (messageModal)) {
   messageButton.addEventListener('click', function (event) {
     showModal(event, messageModal);
+    if (storage) {
+      username.value = storage;
+      email.focus();
+    } else {
+      username.focus();
+    }
+  });
+}
+
+if ((messageModal) && (messageForm)) {
+  messageForm.addEventListener('submit', function (event) {
+    if ((!username.value) || (!email.value)) {
+      event.preventDefault();
+      messageModal.classList.add('modal--error');
+    } else {
+      localStorage.setItem('username', username.value);
+    }
   });
 }
 
@@ -43,13 +70,21 @@ if (modals) {
   [].forEach.call(modals, function (item) {
     var close = item.querySelector('.modal__close');
     close.addEventListener('click', function (event) {
+      event.preventDefault();
       hideModal(item);
     });
     window.addEventListener('keydown', function (event) {
       if (event.keyCode === 27) {
-        hideModal(item);
+        if (item.classList.contains('modal--show')) {
+          hideModal(item);
+        }
       }
     });
+    if (overlay) {
+      overlay.addEventListener('click', function () {
+        hideModal(item);
+      });
+    }
   });
 }
 
