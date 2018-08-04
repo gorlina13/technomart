62cb5354f0ab7b900c67cb6afe8a552db1c413b8
Olga Bazhenova <olgabazhenova07@yandex.ru>
2018-08-04 18:50:55 +0300

    Слайдеры теперь работают на JS

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ var overlay = document.querySelector('.overlay');
 var isStorageSupport = true;
 var storage = '';
 var products = document.querySelectorAll('.product');
+var sliders = document.querySelectorAll('.slider');
 
 function showModal(event, modal) {
   event.preventDefault();
@@ -28,6 +29,109 @@ function hideModal(event, modal) {
   if (overlay) {
     overlay.classList.remove('overlay--show');
   }
+}
+
+function runSlider(slider) {
+  var controls = slider.querySelectorAll('.slider__control');
+  controls = Array.prototype.slice.call(controls);
+  var slides = slider.querySelectorAll('.slider__item');
+  slides = Array.prototype.slice.call(slides);
+  var buttons = slider.querySelectorAll('.slider__button');
+  var buttonsWithin = slider.querySelectorAll('.button');
+
+  function makeControlActive(currentControl) {
+    controls.forEach(function (item) {
+      item.classList.remove('slider__control--active');
+    });
+
+    currentControl.classList.add('slider__control--active');
+  }
+
+  function showSlide(currentControl) {
+    slides.forEach(function (item) {
+      item.classList.remove('slider__item--show');
+    });
+
+    var controlIndex = controls.indexOf(currentControl);
+    if (slides[controlIndex]) {
+      slides[controlIndex].classList.add('slider__item--show');
+    }
+  }
+
+  function setTabindex() {
+    if (buttonsWithin.length > 0) {
+      [].forEach.call(buttonsWithin, function (item) {
+        item.removeAttribute('tabindex');
+        if (!(item.parentElement.classList.contains('slider__item--show'))) {
+          item.setAttribute('tabindex', -1);
+        }
+      });
+    }
+  }
+
+  function disableSlideButton(currentControl) {
+    if (buttons.length > 0) {
+      [].forEach.call(buttons, function (item) {
+        if (item.disabled) {
+          item.disabled = false;
+        }
+      });
+
+      if (currentControl === controls[0]) {
+        buttons[0].disabled = true;
+        // buttons[0].nextElementSibling.focus();
+      }
+
+      if (currentControl === controls[controls.length - 1]) {
+        buttons[buttons.length - 1].disabled = true;
+        // buttons[buttons.length - 1].previousElementSibling.focus();
+      }
+    }
+  }
+
+  function syncSliderElements(currentControl) {
+    makeControlActive(currentControl);
+    showSlide(currentControl);
+    setTabindex();
+    disableSlideButton(currentControl);
+  }
+
+  setTabindex();
+
+  controls.forEach(function (item) {
+    item.addEventListener('click', function (event) {
+      event.preventDefault();
+      syncSliderElements(item);
+    });
+  });
+
+  if (buttons) {
+    [].forEach.call(buttons, function (item) {
+      item.addEventListener('click', function (event) {
+        event.preventDefault();
+        var shownSlide = slider.querySelector('.slider__item--show');
+        var shownSlideIndex = slides.indexOf(shownSlide);
+        if (item.classList.contains('slider__button--prev')) {
+          var control = controls[shownSlideIndex - 1];
+        } else {
+          var control = controls[shownSlideIndex + 1];
+        }
+        syncSliderElements(control);
+        if (control === controls[controls.length - 1]){
+          control.focus();
+        }
+        if (control === controls[0]) {
+          controls[1].focus();
+        }
+      });
+    });
+  }
+}
+
+if (sliders) {
+  [].forEach.call(sliders, function (item) {
+    runSlider(item);
+  });
 }
 
 if ((mapLink) && (mapModal)) {
